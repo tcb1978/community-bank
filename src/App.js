f814b4544294dfd6c7c4fdacbac092bb00af5303
Matthew Eldredge <mattheweldredge@gmail.com>
2018-01-11 10:47:49 -0700

    add routing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { Route } from 'react-router-dom'
+// import { AccountInfo } from './components/AccountInfo'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="">
+        <Route exact path="/" component={Home} />
+        <Route path="/private" component={AccountInfo} />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
